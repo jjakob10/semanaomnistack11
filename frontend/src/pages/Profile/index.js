09359bda2d43edbd0bc17ef0b2da261b756dcee9
jjakob10 <jjakobschmitz@gmail.com>
2020-03-27 12:19:03 -0300

    FrontEnd Parcial 59

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import logoImg from "../../assets/logo.svg"
+
+export default function Profile(){
+  return(
+    <div className="profile-container">
+      <header>
+        {/* <img src={logoImg} */}
+      </header>
+    </div>
+  );
+}
